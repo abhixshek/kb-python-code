4aca8b8aa055417657aa56c3f879bba833368e83
abhixshek <abhishek.dce.2000@gmail.com>
2023-03-26 21:14:16 +0530

    numeric types

--- numeric_types_and_operators.py
+++ numeric_types_and_operators.py
@@ -25,3 +25,15 @@ b = 2 + 3.5j
 c = a + b
 print(c, type(c))
 
+#evaluations of some mixed expressions
+a = 2 ** 3 ** 2 # computes 3 ** 2 first, i.e. = 9 and then 2 ** 9 = 512
+b = 2 ** 3 + 1 ** 2 # since exponentiation has higher precedence than +, it evaluates 1**2=1 and 2**3=8
+# and then adds the results 8+1 = 9
+print(a, b)
+
+a = 4 * 5 / 2 #since * and / are in the same row, they have same precedence and the operations are grouped from left to right
+# therefore 4*5= 20 happens first, then 20 /2 = 10 final result.
+print(a)
+a = 4 / 5 * 2 # 4 / 5 = 0.8 then 0.8*2 = 1.6
+print(a)
+
