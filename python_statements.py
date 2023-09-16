3659108c56f049ded8a9aa710e7a13bf62915d9b
abhixshek <abhishek.dce.2000@gmail.com>
2023-09-16 19:20:51 +0530

    python statements introduction

---
+++ python_statements.py
@@ -0,0 +1,12 @@
+a = 5 # assignment statement
+a, b = 5, 'rt'
+print(a) # function calls are also statements
+print(b)
+
+a , *b = 5, 12, 'rt', (4,5) # notice * mark
+print(a)
+print(b)
+print(type(b)) # list
+
+# if/elif/else , for/while are also statements.
+
